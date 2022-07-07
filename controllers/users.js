ee959123946c8d6098002bc82b25c52e37ea3a8b
Antropov Anton <antropov.a.g@gmail.com>
2022-07-07 12:51:39 +0400

    SameSite cookie

--- controllers/users.js
+++ controllers/users.js
@@ -115,8 +115,8 @@ module.exports.userLogin = (req, res, next) => {
       if (!matched) {
         return next(new WrongEmailOrPasswordError('Неправильный e-mail или пароль'));
       }
-      const token = jwt.sign({ _id: user.id }, JWT_SECRET, { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
+      const token = jwt.sign({ _id: user.id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      res.cookie('jwt', token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, SameSite: 'none' });
       return res.send({ token });
     })
     .catch((error) => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const WrongEmailOrPasswordError = require('../errors/WrongEmailOrPasswordError');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return next(new WrongEmailOrPasswordError('Необходима авторизация - токен не прошёл проверку'));
   }
